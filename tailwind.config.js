15a4e5908f9345982d92e75e1804601485105a3e
Mücahit Şahin <mucahitsahin786@gmail.com>
2021-08-19 21:07:48 +0300

    Edited tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,38 @@
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 // tailwind.config.js
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        myriad: ["Myriad Pro Cond", ...defaultTheme.fontFamily.sans],
+      },
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      supernova: {
+        50: "#fffcf2",
+        100: "#fffae6",
+        200: "#fff2c0",
+        300: "#ffea9b",
+        400: "#ffdb4f",
+        500: "#ffcb04",
+        600: "#e6b704",
+        700: "#bf9803",
+        800: "#997a02",
+        900: "#7d6302",
+      },
+    },
   },
   variants: {
     extend: {},
